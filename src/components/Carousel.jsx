f019e92d2c72faa27043dd31b77c314261ae7a51
Lakshya Jain <minecraftpogtechno@gmail.com>
2023-11-24 23:21:40 -0500

    center of my whole body

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -20,8 +20,20 @@ export default function Carousel() {
         modules={[Pagination]}
       >
         <SwiperSlide>
-            <div className="cara-book-container"></div>
-            <img src="/rr.webp" className="cara-book"/>
+          <div className="car-container">
+            <img src="/rr.webp" className="cara-book" />
+            <div className="car-left">
+              <h2 className="car-left-week">Week #51</h2>
+              <h1 className="car-left-thisweek">This Week : Siege and Storm</h1>
+              <p className="car-left-quote">
+                “When people say impossible, they usually mean improbable.”
+              </p>
+
+              <button className="car-left-join-button">
+                Join the Challenge
+              </button>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
       </Swiper>
