58687e5e73c081580850fdfc2cc7bef1ad2a8a30
Lakshya Jain <minecraftpogtechno@gmail.com>
2023-11-26 13:01:35 -0500

    yes

--- v2/src/app/page.js
+++ v2/src/app/page.js
@@ -119,18 +119,87 @@ export default function Home() {
 
           {/* Cards */}
           <div className="flex justify-evenly mt-7">
-            <div className="w-[30%] h-80 bg-white rounded-3xl shadow-2xl shadow-white flex flex-col">
+            <div className="card w-[45%] h-96 bg-white rounded-3xl shadow-2xl shadow-white flex flex-col">
+              <div
+                id="images-self-help"
+                className="opacity-0 hidden mt-2 mb-0 justify-center items-center"
+              >
+                <img
+                  src="/perm/self-help-flavour/atomic.jpg"
+                  className="mb-0 opacity-100 -rotate-12 w-[90px] h-[138px]"
+                ></img>
+
+                <img
+                  src="/perm/self-help-flavour/tsaongaf.jpg"
+                  className="mb-0 ml-[-50px] -rotate-6 opacity-100 w-[90px] h-[138px]"
+                ></img>
+
+                <img
+                  src="/perm/self-help-flavour/alc.jpg"
+                  className="mb-0 ml-[-30px] z-40 w-[140px] h-[241px] rounded-3xl shadow-2xl shadow-red-500"
+                ></img>
+
+                <img
+                  src="/perm/self-help-flavour/robin.jpg"
+                  className="mb-0 ml-[-30px] rotate-6 opacity-100 w-[90px] h-[138px]"
+                ></img>
+
+                <img
+                  src="/perm/self-help-flavour/obama.jpg"
+                  className="mb-0 ml-[-50px] rotate-12 opacity-100 w-[90px] h-[138px]"
+                ></img>
+              </div>
+
+              <div
+                id="images-self-help"
+                className="opacity-1 mt-2 mb-0 scale-110 flex justify-center items-center z-50"
+              >
+                <img
+                  src="/perm/self-help-flavour/atomic.jpg"
+                  className="mb-0 opacity-100 w-[140px] h-[241px] mr-10"
+                ></img>
+
+                <img
+                  src="/perm/self-help-flavour/tsaongaf.jpg"
+                  className="mb-0  opacity-100 w-[140px] h-[241px] mr-10"
+                ></img>
+
+                <img
+                  src="/perm/self-help-flavour/alc.jpg"
+                  className="mb-0 z-40 w-[140px] h-[241px] mr-10"
+                ></img>
+
+                <img
+                  src="/perm/self-help-flavour/robin.jpg"
+                  className="mb-0 opacity-100 w-[140px] h-[241px] mr-10"
+                ></img>
+
+                <img
+                  src="/perm/self-help-flavour/obama.jpg"
+                  className="mb-0 opacity-100 w-[140px] h-[241px]"
+                ></img>
+              </div>
+
+              <div className="mt-6">
+                <h1 className="text-black text-center font-bold text-6xl">
+                  Self-Help
+                </h1>
+                <p className="text-center font-bold">4 books / month</p>
+              </div>
+            </div>
+
+            <div className="card w-[45%] h-80 bg-white rounded-3xl shadow-2xl shadow-white flex flex-col">
               <div
                 id="images-self-help"
                 className="mt-2 mb-0 flex justify-center items-center"
               >
                 <img
-                  src="/perm/self-help-flavour/robin.jpg"
-                  className="mb-0 opacity-100 w-[90px] h-[138px]"
+                  src="/perm/self-help-flavour/atomic.jpg"
+                  className="mb-0 opacity-100 -rotate-12 w-[90px] h-[138px]"
                 ></img>
                 <img
-                  src="/perm/self-help-flavour/the-monk.jpg"
-                  className="mb-0 ml-[-50px] opacity-100 w-[90px] h-[138px]"
+                  src="/perm/self-help-flavour/tsaongaf.jpg"
+                  className="mb-0 ml-[-50px] -rotate-6 opacity-100 w-[90px] h-[138px]"
                 ></img>
                 <img
                   src="/perm/self-help-flavour/alc.jpg"
@@ -141,7 +210,7 @@ export default function Home() {
                   className="mb-0 ml-[-30px] rotate-6 opacity-100 w-[90px] h-[138px]"
                 ></img>
                 <img
-                  src="/perm/self-help-flavour/the-monk.jpg"
+                  src="/perm/self-help-flavour/obama.jpg"
                   className="mb-0 ml-[-50px] rotate-12 opacity-100 w-[90px] h-[138px]"
                 ></img>
               </div>
@@ -153,8 +222,6 @@ export default function Home() {
                 <p className="text-center">4 books / month</p>
               </div>
             </div>
-            <div></div>
-            <div></div>
           </div>
         </section>
       </section>
--- v2/src/components/Cursor.jsx
+++ v2/src/components/Cursor.jsx
@@ -2,29 +2,61 @@
 
 import { useRef, useEffect } from "react";
 
+export const INDEX_TO_CLASSES = [
+  [
+    ["-rotate-12", "w-[90px]", "h-[138px]"],
+    ["mr-10", "h-[241px]", "w-[140px]"],
+  ],
+  [
+    ["-rotate-6", "w-[90px]", "h-[138px]", "ml-[-50px]"],
+    ["mr-10", "h-[241px]", "w-[140px]"],
+  ],
+  [
+    ["rounded-3xl", "shadow-2xl", "shadow-red-500", "ml-[-30px]"],
+    [""],
+  ],
+  [
+    ["-rotate-6", "w-[90px]", "h-[138px]", "ml-[-50px]"],
+    ["mr-10", "h-[241px]", "w-[140px]"],
+  ],
+  [
+    ["-rotate-6", "w-[90px]", "h-[138px]", "ml-[-50px]"],
+    ["mr-10", "h-[241px]", "w-[140px]"],
+  ],
+];
+
 export default function Cursor() {
   const cursor = useRef();
 
   useEffect(() => {
-    window.addEventListener("mousemove", move)
+    window.addEventListener("mousemove", move);
 
     // Get all links
     const links = document.querySelectorAll("a");
     for (let m = 0; m < links.length; m++) {
-        const link = links[m];
-        link.addEventListener("mouseenter", onLink);
-        link.addEventListener("mouseout", outLink)
+      const link = links[m];
+      link.addEventListener("mouseenter", onLink);
+      link.addEventListener("mouseout", outLink);
+    }
+
+    // Get all cards
+    const cards = document.getElementsByClassName("card");
+    for (let k = 0; k < cards.length; k++) {
+      const card = cards[k];
+      card.addEventListener("mouseenter", onCard);
+      card.addEventListener("mouseleave", outCard);
     }
 
     // Get all images
     const images = document.querySelectorAll("img");
     for (let n = 0; n < images.length; n++) {
-      const img = images[n];
-      img.addEventListener("mouseenter", onImg);
-      img.addEventListener("mouseout", outImg)
+      // Add some kind of effect man
+      // const img = images[n];
+      // img.addEventListener("mouseenter", onImg);
+      // img.addEventListener("mouseout", outImg)
     }
-  }, [])
-  
+  }, []);
+
   function move(e) {
     console.log("hi");
     const mouseX = e.clientX;
@@ -34,29 +66,45 @@ export default function Cursor() {
     cursor.current.style.left = `${mouseX}px`;
   }
 
-  function onLink(){
+  function onLink() {
     cursor.current.classList.add("custom-cursor--link");
   }
 
-  function outLink(){
+  function outLink() {
     cursor.current.classList.remove("custom-cursor--link");
   }
 
-  function onImg(){
+  function onCard(e) {
+    // Update Cursor
+    cursor.current.classList.add("custom-cursor--link");
+
+    // Get Image Holder
+    const image_holder = e.target.firstChild();
+    image_holder.classList.add("z-50");
+
+    // Get All Images
+    const images = image_holder.children;
+    for (let n = 0; n < images.length; n++) {
+      const img = images[n];
+    }
+  }
+
+  function outCard(e) {
+    console.log(e.target);
+    cursor.current.classList.remove("custom-cursor--link");
+  }
+
+  function onImg() {
     cursor.current.classList.add("custom-cursor--img");
   }
 
-  function outImg(){
+  function outImg() {
     cursor.current.classList.remove("custom-cursor--img");
   }
 
   return (
     <>
-      <div
-        ref={cursor}
-        id="curs"
-        className="custom-cursor"
-      ></div>
+      <div ref={cursor} id="curs" className="custom-cursor"></div>
     </>
   );
 }
