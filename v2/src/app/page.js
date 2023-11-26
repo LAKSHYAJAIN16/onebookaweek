2740b7aed7cd415ec602186d3b4301cbde2dd098
Lakshya Jain <minecraftpogtechno@gmail.com>
2023-11-26 10:43:35 -0500

    modal... ?

--- v2/src/app/page.js
+++ v2/src/app/page.js
@@ -31,57 +31,64 @@ export default function Home() {
           do you have what it takes?
         </h1>
         <p className="text-center font-main text-white text-4xl mt-3 font-bold">
-          take your life back. book by book.
+          take your life back. week by week. book by book.
         </p>
       </section>
 
       {/* Carousel */}
-      <section className="mt-28 font-main flex justify-center scale-100">
-        {/* Left */}
-        <div>
-          {/* Head */}
-          <section>
-            <h1 className="text-3xl font-bold text-red-500">#31</h1>
+      <div className="w-full flex justify-center">
+        <section className="mt-28 font-main flex justify-center scale-100 bg-toned shadow-2xl shadow-white pl-32 pr-32 pt-14 pb-14">
+          {/* Left */}
+          <div>
+            {/* Head */}
+            <section>
+              <h1 className="text-3xl font-bold text-red-500">#31</h1>
 
-            {/* Stars */}
-            {/* <div className="mt-3 mb-0 flex">
-              <img src="/star.png" className="w-4"/>
-              <img src="/star.png" className="w-4"/>
-              <img src="/star.png" className="w-4"/>
-              <img src="/half-star.png" className="w-4"/>
-              <img src="/gray-boi.png" className="w-4"/>
-            </div> */}
-            
-            <p className=" text-6xl font-logo font-bold text-white">Siege and Storm</p>
-            <p className="text-sm text-white font-bold text-right">
-              Leigh Bardugo
-            </p>
-          </section>
+              {/* Stars */}
+              <div className="mt-3 mb-0 flex">
+                <img src="/star.png" className="w-4" />
+                <img src="/star.png" className="w-4" />
+                <img src="/star.png" className="w-4" />
+                <img src="/half-star.png" className="w-4" />
+                <img src="/gray-boi.png" className="w-4" />
+              </div>
 
-          {/* The genres */}
-          <section className="mt-5">
-            <p className="font-logo-purple text-3xl">
-              fantasy. romance. love story.
-            </p>
-          </section>
+              <p className=" text-6xl font-logo font-bold text-white">
+                Siege and Storm
+              </p>
+              <p className="text-sm text-white font-bold text-right">
+                Leigh Bardugo
+              </p>
+            </section>
 
-          {/* Counter */}
-          <section>
-            <br />
-            <p className="font-bold text-white text-3xl">
-              {copydeliveredAm.toLocaleString()} copies delivered
-            </p>
-            <p className="font-bold text-white text-3xl">
-              {discussionPostAm.toLocaleString()} discussion posts
-            </p>
-          </section>
-        </div>
+            {/* The genres */}
+            <section className="mt-5">
+              <p className="font-logo-purple text-3xl">
+                fantasy. romance. love story.
+              </p>
+            </section>
 
-        {/* Image */}
-        <div>
-          <img src="/siege.jpg" className="w-60 ml-20 hover:scale-125 hover:rounded-2xl transition-all"></img>
-        </div>
-      </section>
+            {/* Counter */}
+            <section>
+              <br />
+              <p className="font-bold text-white text-3xl">
+                {copydeliveredAm.toLocaleString()} copies delivered
+              </p>
+              <p className="font-bold text-white text-3xl">
+                {discussionPostAm.toLocaleString()} discussion posts
+              </p>
+            </section>
+          </div>
+
+          {/* Image */}
+          <div>
+            <img
+              src="/siege.jpg"
+              className="w-60 ml-20 hover:scale-125 hover:rounded-2xl transition-all"
+            ></img>
+          </div>
+        </section>
+      </div>
 
       {/* The Goal */}
       <section className="mt-[55px]">
