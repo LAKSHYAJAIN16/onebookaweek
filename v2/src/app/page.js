442f84093c4c68d1c969f1760adf971294fb8cf1
Lakshya Jain <minecraftpogtechno@gmail.com>
2023-11-28 10:02:13 -0500

    yayayayayay school!

--- v2/src/app/page.js
+++ v2/src/app/page.js
@@ -1,28 +1,6 @@
-"use client";
-
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [discussionPostAmount, setDiscussionPostAmount] = useState(18000);
-  const [copydeliveredAmount, setCopydeliveredAmount] = useState(12000);
-  const [discussionPostAm, setDiscussionPostAm] = useState(18000);
-  const [copydeliveredAm, setCopydeliveredAm] = useState(12000);
-
-  useEffect(() => {
-    // Optimize this b!tch
-    // if (discussionPostAmount > discussionPostAm) {
-    //   setDiscussionPostAm(
-    //     discussionPostAm + Math.floor(discussionPostAmount / RATE_CONSTANT)
-    //   );
-    // }
-    // if (copydeliveredAmount > copydeliveredAm) {
-    //   setCopydeliveredAm(
-    //     copydeliveredAm + Math.floor(copydeliveredAmount / RATE_CONSTANT)
-    //   );
-    // }
-  }, []);
-
   return (
     <div>
       {/* Epic Header */}
@@ -35,25 +13,18 @@ export default function Home() {
         </p>
       </section>
 
-      {/* 52 weeks. 52 books */}
-      <section className="mt-[80px]">
-        <p className="text-center font-logo text-white text-8xl mt-8 font-bold">
-          who are we?
-        </p>
-        <p className="text-center font-main font-bold text-white text-3xl">
-          we're a subscription service aimed at connecting bookworms and making reading as fun as possible.
-        </p>
-      </section>
-
       {/* The Challenge */}
       <section className="mt-[80px]">
         <p className="text-center font-logo-green text-white text-8xl mt-8 font-bold">
           the challenge
         </p>
 
-        <p className="text-center font-main text-red-500 text-4xl mt-3 font-bold">
-          WARNING : this challenge not for the timid hearted.
-        </p>
+        <div className="text-center font-main text-red-500 text-4xl mt-5 font-bold rounded-2xl ">
+          <div className="flex justify-center items-center border-2 border-red-500 w-[70%] ml-auto mr-auto pt-3 pb-3 pr-3 pl-3 rounded-3xl">
+            <Image src="/alert.png" width={40} height={30} />
+            <span> WARNING : this challenge not for the timid hearted. </span>
+          </div>
+        </div>
       </section>
     </div>
   );
