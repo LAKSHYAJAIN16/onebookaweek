c66e3628a76bdc1d696b59bf4f42a16799950261
Lakshya Jain <minecraftpogtechno@gmail.com>
2023-12-01 10:41:02 -0500

    hi!

--- v2/src/app/book/[id]/page.js
+++ v2/src/app/book/[id]/page.js
@@ -2,7 +2,7 @@ export default function Page({ params }) {
   return (
     <div className="flex ml-5 justify-evenly">
       <div>
-        <h1 className="font-logo text-7xl">Siege and Storm</h1>
+        <h1 className="font-logotext-7xl">Siege and Storm</h1>
         <p className="text-white font-bold mt-2 text-2xl">by Leigh Bardugo</p>
         <button className="mt-7 text-white font-bold text-4xl hover:cursor-none">Day 1 Review ⬇️</button>
       </div>
